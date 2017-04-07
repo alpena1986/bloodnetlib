5f099cb2d291393179d7761975d730e26dafac38
alpena <alpena@192.168.1.7>
2017-04-07 18:41:46 +0900

    类完善

--- bloodnetlib/src/com/bloodnet/lib/Profile.java
+++ bloodnetlib/src/com/bloodnet/lib/Profile.java
@@ -4,6 +4,56 @@ public class Profile {
 
 	private String id;
 
+	private String sex;
+	
+	private String familyName;
+	
+	private String firstName;
+	
+	private String birthdate;
+	
+	private String idNumber;
+	
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getFamilyName() {
+		return familyName;
+	}
+
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+
 	public String getId() {
 		return id;
 	}
